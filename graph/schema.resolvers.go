f3fe7c110cbde0614fac03fb81bb8b81453f149d
Mark Wen <markwenguojie94@gmail.com>
2020-04-28 17:41:58 -0700

    Updated schemas to match DB and removed Experience

--- graph/schema.resolvers.go
+++ graph/schema.resolvers.go
@@ -14,15 +14,11 @@ func (r *mutationResolver) AddEducation(ctx context.Context, education model.Edu
 	panic(fmt.Errorf("not implemented"))
 }
 
+func (r *mutationResolver) UpdateEducation(ctx context.Context, id string, education model.EducationInput) (*model.Education, error) {
+	panic(fmt.Errorf("not implemented"))
+}
+
 func (r *mutationResolver) DeleteEducation(ctx context.Context, id string, styleID string) (*model.Education, error) {
-	panic(fmt.Errorf("not implemented"))
-}
-
-func (r *mutationResolver) AddExperience(ctx context.Context, experience model.ExperienceInput, style model.StyleInput) (*model.Experience, error) {
-	panic(fmt.Errorf("not implemented"))
-}
-
-func (r *mutationResolver) DeleteExperience(ctx context.Context, id string, styleID string) (*model.Experience, error) {
 	panic(fmt.Errorf("not implemented"))
 }
 
@@ -40,14 +36,6 @@ func (r *queryResolver) GetEducationByID(ctx context.Context, id string) (*model
 	panic(fmt.Errorf("not implemented"))
 }
 
-func (r *queryResolver) AllExperiences(ctx context.Context) ([]*model.Experience, error) {
-	panic(fmt.Errorf("not implemented"))
-}
-
-func (r *queryResolver) GetExperienceByID(ctx context.Context, id string) (*model.Experience, error) {
-	panic(fmt.Errorf("not implemented"))
-}
-
 // Mutation returns generated.MutationResolver implementation.
 func (r *Resolver) Mutation() generated.MutationResolver { return &mutationResolver{r} }
 
@@ -56,16 +44,3 @@ func (r *Resolver) Query() generated.QueryResolver { return &queryResolver{r} }
 
 type mutationResolver struct{ *Resolver }
 type queryResolver struct{ *Resolver }
-
-// !!! WARNING !!!
-// The code below was going to be deleted when updating resolvers. It has been copied here so you have
-// one last chance to move it out of harms way if you want. There are two reasons this happens:
-//  - When renaming or deleting a resolver the old code will be put in here. You can safely delete
-//    it when you're done.
-//  - You have helper methods in this file. Move them out to keep these resolver files clean.
-func (r *mutationResolver) UpdateEducation(ctx context.Context, id string, education model.EducationInput) (*model.Education, error) {
-	panic(fmt.Errorf("not implemented"))
-}
-func (r *mutationResolver) UpdateExperience(ctx context.Context, id string, experience model.ExperienceInput) (*model.Experience, error) {
-	panic(fmt.Errorf("not implemented"))
-}
